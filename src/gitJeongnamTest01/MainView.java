eb8ccbbba07790925e937c2edf22e4b4e017bf80
82104 <82104@DESKTOP-D8PJ142>
2021-03-12 10:39:51 +0900

    첫번째 세팅하기

---
+++ src/gitJeongnamTest01/MainView.java
@@ -0,0 +1,10 @@
+package gitJeongnamTest01;
+
+public class MainView {
+
+	public static void main(String[] args) {
+		System.out.println("git Ω√¿€");
+
+	}
+
+}
---
+++ src/mvc/controller/TestController.java
@@ -0,0 +1,7 @@
+package mvc.controller;
+
+public class TestController {
+	public void insert() {
+		System.out.println("insert±â´É");
+	}
+}
